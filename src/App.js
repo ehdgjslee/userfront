6112069a870bf2175d197909123aaf031b805ef4
ehdgjslee <wkdak2164@gmail.com>
2022-10-26 22:52:22 +0900

    init: react프로젝트 생성

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+import Home from './pages/Home';
+import About from './pages/About';
+
+function App(){
+  return (
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/about' element={<About />} />
+        </Routes>
+      </Router>
+  );
+}
+
+
+
+export default App;
